f24a201d72ed27847fd9406f1f84585fb4c4b4ce
Stanley Hua <stanley.hua@mail.utoronto.ca>
2022-04-10 12:42:57 -0400

    Fix corner pings

--- funnel/algo_strategy.py
+++ funnel/algo_strategy.py
@@ -52,59 +52,25 @@ class AlgoStrategy(gamelib.AlgoCore):
         # This is a good place to do initial setup
         self.scored_on_locations = set()
 
-        # Walls/turrets should we have at any point
+        # Accumulators for structures we should have at a given point
         self.P1_WALLS_EXPECTED = {}
         self.P1_WALLS_OPTIONAL = {}
         self.P1_TURRET_EXPECTED = {}
         self.P1_SUPPORT_EXPECTED = set()
+        self.total_support = 0
 
-        # For efficient look-up of SPAWN EDGES
-        global LEFT_SPAWN_EDGES, RIGHT_SPAWN_EDGES
-        temp_left, temp_right = gamelib.game_map.GameMap(self.config).get_edges()[2:]
+        # Define core structures needed before attacking
+        self.core_structures = {
+            "WALL_1": {(2, 11), (3, 10), (4, 9), (20, 9), (5, 8), (19, 8), (6, 7), (8, 7), (9, 7), (10, 7), (11, 7), (12, 7), (13, 7), (14, 7), (15, 7), (16, 7), (17, 7), (18, 7), (7, 6)},
+            "WALL_2": {(0, 13), (1, 13), (2, 12), (25, 13), (22, 12), (24, 13)},
+            "TURRET_1": {(22, 10), (24, 10)},
+            "TURRET_2": {(1, 12), (21, 10), (22, 11), (24, 12)}}
 
-        LEFT_SPAWN_EDGES = set([tuple(loc) for loc in temp_left])
-        RIGHT_SPAWN_EDGES = set([tuple(loc) for loc in temp_right])
-
-        # Define regions for player 1 for later
-        global P1_REGION_1, P1_REGION_2, P1_REGION_3, P1_REGION_4, P1_REGION_5, P1_REGION_6, P1_REGION_7, P1_REGION_8
-        global P1_COORD_TO_REGION
-        P1_REGION_1 = {(0, 13), (1, 13), (2, 13), (3, 13), (4, 13), (5, 13), (6, 13), (7, 13), (1, 12), (2, 12), (3, 12), (4, 12), (5, 12), (6, 12), (7, 12), (2, 11), (3, 11), (4, 11), (5, 11), (6, 11), (7, 11), (3, 10), (4, 10), (5, 10), (6, 10), (7, 10)}
-        P1_REGION_2 = {(8, 13), (9, 13), (10, 13), (11, 13), (12, 13), (13, 13), (8, 12), (9, 12), (10, 12), (11, 12), (12, 12), (13, 12), (8, 11), (9, 11), (10, 11), (11, 11), (12, 11), (13, 11), (8, 10), (9, 10), (10, 10), (11, 10), (12, 10), (13, 10)}
-        P1_REGION_3 = {(14, 13), (15, 13), (16, 13), (17, 13), (18, 13), (19, 13), (14, 12), (15, 12), (16, 12), (17, 12), (18, 12), (19, 12), (14, 11), (15, 11), (16, 11), (17, 11), (18, 11), (19, 11), (14, 10), (15, 10), (16, 10), (17, 10), (18, 10), (19, 10)}
-        P1_REGION_4 = {(20, 13), (21, 13), (22, 13), (23, 13), (24, 13), (25, 13), (26, 13), (27, 13), (20, 12), (21, 12), (22, 12), (23, 12), (24, 12), (25, 12), (26, 12), (20, 11), (21, 11), (22, 11), (23, 11), (24, 11), (25, 11), (20, 10), (21, 10), (22, 10), (23, 10), (24, 10)}
-        P1_REGION_5 = {(4, 9), (5, 9), (6, 9), (7, 9), (8, 9), (9, 9), (10, 9), (5, 8), (6, 8), (7, 8), (8, 8), (9, 8), (10, 8), (6, 7), (7, 7), (8, 7), (9, 7), (10, 7), (7, 6), (8, 6), (9, 6), (10, 6), (8, 5), (9, 5), (10, 5), (9, 4), (10, 4), (10, 3)}
-        P1_REGION_6 = {(17, 9), (18, 9), (19, 9), (20, 9), (21, 9), (22, 9), (23, 9), (17, 8), (18, 8), (19, 8), (20, 8), (21, 8), (22, 8), (17, 7), (18, 7), (19, 7), (20, 7), (21, 7), (17, 6), (18, 6), (19, 6), (20, 6), (17, 5), (18, 5), (19, 5), (17, 4), (18, 4), (17, 3)}
-        P1_REGION_7 = {(11, 6), (12, 6), (13, 6), (14, 6), (15, 6), (16, 6), (11, 5), (12, 5), (13, 5), (14, 5), (15, 5), (16, 5), (11, 4), (12, 4), (13, 4), (14, 4), (15, 4), (16, 4), (11, 3), (12, 3), (13, 3), (14, 3), (15, 3), (16, 3), (11, 2), (12, 2), (13, 2), (14, 2), (15, 2), (16, 2), (12, 1), (13, 1), (14, 1), (15, 1), (13, 0), (14, 0)}
-        P1_REGION_8 = {(11, 9), (12, 9), (13, 9), (14, 9), (15, 9), (16, 9), (11, 8), (12, 8), (13, 8), (14, 8), (15, 8), (16, 8), (11, 7), (12, 7), (13, 7), (14, 7), (15, 7), (16, 7)}
-
-        p1_regions = [P1_REGION_1, P1_REGION_2, P1_REGION_3, P1_REGION_4, P1_REGION_5, P1_REGION_6, P1_REGION_7, P1_REGION_8]
-        P1_COORD_TO_REGION = {}
-        for i in range(8):
-            for tup in p1_regions[i]:
-                P1_COORD_TO_REGION[tup] = i + 1
-
-        # Define regions for player 2 for later
-        global P2_REGION_1, P2_REGION_2, P2_REGION_3, P2_REGION_4, P2_REGION_5, P2_REGION_6, P2_REGION_7, P2_REGION_8
-        global P2_COORD_TO_REGION
-        P2_REGION_1 = {(3, 17), (4, 17), (5, 17), (6, 17), (7, 17), (2, 16), (3, 16), (4, 16), (5, 16), (6, 16), (7, 16), (1, 15), (2, 15), (3, 15), (4, 15), (5, 15), (6, 15), (7, 15), (0, 14), (1, 14), (2, 14), (3, 14), (4, 14), (5, 14), (6, 14), (7, 14)}
-        P2_REGION_2 = {(8, 17), (9, 17), (10, 17), (11, 17), (12, 17), (13, 17), (8, 16), (9, 16), (10, 16), (11, 16), (12, 16), (13, 16), (8, 15), (9, 15), (10, 15), (11, 15), (12, 15), (13, 15), (8, 14), (9, 14), (10, 14), (11, 14), (12, 14), (13, 14)}
-        P2_REGION_3 = {(14, 17), (15, 17), (16, 17), (17, 17), (18, 17), (19, 17), (14, 16), (15, 16), (16, 16), (17, 16), (18, 16), (19, 16), (14, 15), (15, 15), (16, 15), (17, 15), (18, 15), (19, 15), (14, 14), (15, 14), (16, 14), (17, 14), (18, 14), (19, 14)}
-        P2_REGION_4 = {(20, 17), (21, 17), (22, 17), (23, 17), (24, 17), (20, 16), (21, 16), (22, 16), (23, 16), (24, 16), (25, 16), (20, 15), (21, 15), (22, 15), (23, 15), (24, 15), (25, 15), (26, 15), (20, 14), (21, 14), (22, 14), (23, 14), (24, 14), (25, 14), (26, 14), (27, 14)}
-        P2_REGION_5 = {(10, 24), (9, 23), (10, 23), (8, 22), (9, 22), (10, 22), (7, 21), (8, 21), (9, 21), (10, 21), (6, 20), (7, 20), (8, 20), (9, 20), (10, 20), (5, 19), (6, 19), (7, 19), (8, 19), (9, 19), (10, 19), (4, 18), (5, 18), (6, 18), (7, 18), (8, 18), (9, 18), (10, 18)}
-        P2_REGION_6 = {(17, 24), (17, 23), (18, 23), (17, 22), (18, 22), (19, 22), (17, 21), (18, 21), (19, 21), (20, 21), (17, 20), (18, 20), (19, 20), (20, 20), (21, 20), (17, 19), (18, 19), (19, 19), (20, 19), (21, 19), (22, 19), (17, 18), (18, 18), (19, 18), (20, 18), (21, 18), (22, 18), (23, 18)}
-        P2_REGION_7 = {(13, 27), (14, 27), (12, 26), (13, 26), (14, 26), (15, 26), (11, 25), (12, 25), (13, 25), (14, 25), (15, 25), (16, 25), (11, 24), (12, 24), (13, 24), (14, 24), (15, 24), (16, 24), (11, 23), (12, 23), (13, 23), (14, 23), (15, 23), (16, 23), (11, 22), (12, 22), (13, 22), (14, 22), (15, 22), (16, 22), (11, 21), (12, 21), (13, 21), (14, 21), (15, 21), (16, 21)}
-        P2_REGION_8 = {(11, 20), (12, 20), (13, 20), (14, 20), (15, 20), (16, 20), (11, 19), (12, 19), (13, 19), (14, 19), (15, 19), (16, 19), (11, 18), (12, 18), (13, 18), (14, 18), (15, 18), (16, 18)}
-
-        p2_regions = [P2_REGION_1, P2_REGION_2, P2_REGION_3, P2_REGION_4, P2_REGION_5, P2_REGION_6, P2_REGION_7, P2_REGION_8]
-        P2_COORD_TO_REGION = {}
-        for i in range(8):
-            for tup in p2_regions[i]:
-                P2_COORD_TO_REGION[tup] = i + 1
-
+        # Corner Ping config
         init_attack_method_globals(config)
         self.corner_ping_attack = CornerPing()
 
-        self.total_support = 0
+        
 
     def get_attack_spawns(self, game_state, method):
         """
@@ -121,7 +87,6 @@ class AlgoStrategy(gamelib.AlgoCore):
                 return None
             if hole in optional_walls:
                 min_bank -= game_state.type_cost(WALL)[game_state.SP]
-
 
         new_structs = method.get_new_structures(game_state, min_bank)
 
@@ -260,12 +225,8 @@ class AlgoStrategy(gamelib.AlgoCore):
         self.repair_defences(game_state)
         
         if not alt_defense:
-            # First, place basic defenses
-            self.build_defences(game_state, block_right=True)
-
-            self.remove_walls_lvl1(game_state)
-
             holes = set()
+            enforced_locs = None
             if game_state.turn_number < 5:
                 game_state.attempt_spawn(INTERCEPTOR, (19, 15))
             # elif game_state.get_resource(1, 1) >= 15:
@@ -280,6 +241,13 @@ class AlgoStrategy(gamelib.AlgoCore):
                     holes = self.corner_ping_attack.get_holes(game_state)
                     self.perform_attack(game_state, self.corner_ping_attack, spawns)
 
+                    # Specify that only core structures can be built when attacking
+                    enforced_locs = self.core_structures['WALL_1'].union(self.core_structures['WALL_2'])
+                    enforced_locs = enforced_locs.union(self.core_structures['TURRET_1']).union(self.core_structures['TURRET_2'])
+                    enforced_locs = enforced_locs.difference(holes)
+
+            self.build_defences(game_state, block_right=False, enforced_locs=enforced_locs)
+            self.remove_walls_lvl1(game_state)
             self.patch_optional_walls(game_state, holes)
 
             # Lastly, if we have spare SP, let's build some supports
@@ -369,10 +337,13 @@ class AlgoStrategy(gamelib.AlgoCore):
             else:
                 self.P1_TURRET_EXPECTED[loc] = 2
 
-    def build_defences(self, game_state, block_right=False):
+    def build_defences(self, game_state, block_right=False, enforced_locs=None):
         """
         Build funnel defense.
         """
+        def formatter(unit_type, locations):
+            return [(loc, unit_type) for loc in locations]
+
         turn_number = game_state.turn_number
 
         turrets_loc = []
@@ -486,6 +457,14 @@ class AlgoStrategy(gamelib.AlgoCore):
             to_upgrade.extend(upgrade_12)
             # We don't mark it as upgraded, so when repairing, we don't prioritize its upgrade
 
+
+        # Enforce restriction
+        if enforced_locs is not None:
+            turrets_loc = [t for t in turrets_loc if t in enforced_locs]
+            walls_loc = [w for w in walls_loc if w in enforced_locs]
+            to_upgrade = [u for u in to_upgrade if u in enforced_locs]
+            supports_loc = [s for s in supports_loc if s in enforced_locs]
+
         # Turn 1 Turrets & Walls
         game_state.attempt_spawn(TURRET, turrets_loc)
         game_state.attempt_spawn(WALL, walls_loc)
@@ -527,10 +506,10 @@ class AlgoStrategy(gamelib.AlgoCore):
         turret = TURRET
 
         # Necessary structures and their levels
-        walls_lvl1 = {(2, 11), (3, 10), (4, 9), (20, 9), (5, 8), (19, 8), (6, 7), (8, 7), (9, 7), (10, 7), (11, 7), (12, 7), (13, 7), (14, 7), (15, 7), (16, 7), (17, 7), (18, 7), (7, 6)}
-        walls_lvl2 = {(0, 13), (1, 13), (2, 12), (25, 13), (22, 12), (24, 13)}
-        turrets_lvl1 = {(22, 10), (24, 10)}
-        turrets_lvl2 = {(1, 12), (21, 10), (22, 11), (24, 12)}
+        walls_lvl1 = self.core_structures['WALL_1']
+        walls_lvl2 = self.core_structures['WALL_1']
+        turrets_lvl1 = self.core_structures['TURRET_1']
+        turrets_lvl2 = self.core_structures['TURRET_2']
 
         # Remove ignored locations
         if ignore_locations is not None:
